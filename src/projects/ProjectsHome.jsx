cbef10f9207f9be459feac16a6d52f2855988893
Pranav Mandhare <64312310+pran-av@users.noreply.github.com>
2023-03-07 19:03:55 +0000

    Trying to render cards with useState

--- src/projects/ProjectsHome.jsx
+++ src/projects/ProjectsHome.jsx
@@ -1,19 +1,31 @@
+import {Route} from 'react-router-dom';
+//import React, { useState } from 'react';
+
 import CardItem from "../components/CardItem";
 import HTMLNotes from './HTMLNotes';
-import HandsOnHTML from './HandsOnHTML';
+//import HandsOnHTML from './HandsOnHTML';
+//const [isHTMLNotesClicked, setIsHTMLNotesClicked] = useState(false);
 
 const ProjectsHome = () => {
   return (
     <section>
       <h2>This is Projects Home!</h2>
-      
+
+      <>
+        <CardItem
+        path='./HTMLNotes'
+        text='#1 HTML Series: HTML Notes'
+        description='This is the description of the card'
+        //onClick={() => setIsHTMLNotesClicked(true)}
+        />
+        {/*
+        <Route path = '/projects/HTMLNotes' element={<HTMLNotes />} />
+         {isHTMLNotesClicked && <HTMLNotes />} */}
+        
+      </>
+    
       <CardItem
-      path='./HTMLNotes'
-      text='#1 HTML Series: HTML Notes'
-      description='This is the description of the card'
-      />
-      <CardItem
-      path='../projects/HandsOnHTML'
+      path='/HandsOnHTML'
       text='#2 HTML Series: File Structures in an HTML Project'
       description='This is the description of the card'
       />
