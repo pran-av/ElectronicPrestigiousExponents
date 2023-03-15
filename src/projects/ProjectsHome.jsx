837f60d30255cf90df56a49e1db2561a13a471be
Pranav Mandhare <64312310+pran-av@users.noreply.github.com>
2023-03-15 20:28:56 +0000

    Adding Route to App makes the program work. Experimenting with Switch.

--- src/projects/ProjectsHome.jsx
+++ src/projects/ProjectsHome.jsx
@@ -1,37 +1,22 @@
-import {Route} from 'react-router-dom';
-//import React, { useState } from 'react';
+import {Routes, Route, Switch} from 'react-router-dom';
 
 import CardItem from "../components/CardItem";
 import HTMLNotes from './HTMLNotes';
 //import HandsOnHTML from './HandsOnHTML';
-//const [isHTMLNotesClicked, setIsHTMLNotesClicked] = useState(false);
 
-const ProjectsHome = () => {
+export default function ProjectsHome() {
   return (
-    <section>
-      <h2>This is Projects Home!</h2>
-
+    
       <>
+        <h2>Welcome to Projects Home!</h2>
         <CardItem
         path='./HTMLNotes'
         text='#1 HTML Series: HTML Notes'
         description='This is the description of the card'
-        //onClick={() => setIsHTMLNotesClicked(true)}
         />
-        {/*
-        <Route path = '/projects/HTMLNotes' element={<HTMLNotes />} />
-         {isHTMLNotesClicked && <HTMLNotes />} */}
-        
-      </>
-    
-      <CardItem
-      path='/HandsOnHTML'
-      text='#2 HTML Series: File Structures in an HTML Project'
-      description='This is the description of the card'
-      />
-     
-    </section>     
+        <Switch>
+          <Route path = './HTMLNotes' element={<div>HTML Notes</div>} />
+        </Switch>
+      </>   
   );
 }
-
-export default ProjectsHome;
